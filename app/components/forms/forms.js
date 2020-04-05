763cb494696de048ec400e4037a2c164071141ac
Erin Macalligan <github@unlikelyit.com>
2020-04-05 22:08:34 +0800

    ADD: Work in progress with form custom MailChimp submission.
    Using the mailchimp API to subscribe a new client onto MailChimp audience list.

--- app/components/forms/forms.js
+++ app/components/forms/forms.js
@@ -3,16 +3,38 @@
  * When they are villed, apply class '.js--filled'
  */
 const forms = [...document.querySelectorAll('.form')];
+
+// function check
+
 forms.forEach((form) => {
-	const inputs = [...form.querySelectorAll('input')];
-	const textareas = [...form.querySelectorAll('textarea')];
+	const inputs = [...form.querySelectorAll('input'), ...form.querySelectorAll('textarea')];
 	inputs.forEach((input) => {
 		input.addEventListener('input', () => {
-			if (input.value !== "" && !input.classList.contains('js--filled')) {
+			if (input.value !== '' && !input.classList.contains('js--filled')) {
 				input.classList.add('js--filled');
-			} else if (input.value === "" && input.classList.contains('js--filled')) {
+			} else if (input.value === '' && input.classList.contains('js--filled')) {
 				input.classList.remove('js--filled');
 			}
 		});
+	});
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		let requiredMissing = false;
+
+		inputs.forEach((input) => {
+			if (input.attributes.required && (input.value === '' || !input.checked)) {
+				input.classList.add('js--input-error');
+				requiredMissing = true;
+			} else if (input.type === 'email') {
+
+			}
+		});
+
+		if (!requiredMissing) {
+			fetch('', {
+
+			});
+		}
 	});
 });
